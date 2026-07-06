464e49010fe22e7a8e36e8c8bfd50d67b8b6757c
Kiyarash <Kiyarashhadidian@gmail.com>
2026-07-06 21:17:51 +0330

    feat(threejs): enhance 3D scene with orbit controls and responsive resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,73 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function App() {
 	const containerRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		// پاک‌سازی کامل کانتینر برای جلوگیری از دو کانواس شدن
 		if (containerRef.current) {
 			containerRef.current.innerHTML = "";
 		}
 
+		// Scene
 		const scene = new THREE.Scene();
+
+		// Mesh
 		const geometry = new THREE.SphereGeometry(3, 64, 64);
 		const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
 		const mesh = new THREE.Mesh(geometry, material);
 		scene.add(mesh);
 
-    // Sizes
-    const sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    }
+		// Light
 		const light = new THREE.PointLight(0xffffff, 100, 100);
 		light.position.set(0, 10, 10);
 		scene.add(light);
 
+		// Sizes
+		const sizes = {
+			width: window.innerWidth,
+			height: window.innerHeight,
+		};
+
+		// Camera
 		const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
 		camera.position.z = 20;
-		scene.add(camera);
 
+		// Renderer
 		const renderer = new THREE.WebGLRenderer();
 		renderer.setSize(sizes.width, sizes.height);
-		renderer.render(scene, camera);
+    renderer.setPixelRatio(2);
 		containerRef.current?.appendChild(renderer.domElement);
 
+		// Controls
+		const controls = new OrbitControls(camera, renderer.domElement);
+		controls.enableDamping = true;
+    controls.enablePan = false;
+    controls.enableZoom = false;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 5;
+
+		// Animation Loop
+		const animate = () => {
+			controls.update();
+			renderer.render(scene, camera);
+			requestAnimationFrame(animate);
+		};
+		animate();
+
+		const handleResize = () => {
+			sizes.width = window.innerWidth;
+			sizes.height = window.innerHeight;
+			camera.aspect = sizes.width / sizes.height;
+			camera.updateProjectionMatrix();
+			renderer.setSize(sizes.width, sizes.height);
+		};
+		window.addEventListener("resize", handleResize);
+
 		return () => {
+			// Cleanup
+			window.removeEventListener("resize", handleResize);
 			if (containerRef.current?.contains(renderer.domElement)) {
 				containerRef.current.removeChild(renderer.domElement);
 			}
@@ -42,11 +75,7 @@ function App() {
 		};
 	}, []);
 
-	return (
-		<>
-			<div ref={containerRef} />
-		</>
-	);
+	return <div ref={containerRef} />;
 }
 
 export default App;
